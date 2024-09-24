55db09fdcfeabedd428e865f33e1e06d7473c091
Bougou <bougou@126.com>
2024-09-24 19:54:13 +0800

    move index.yaml to home

--- scripts/make-index.sh
+++ scripts/make-index.sh
@@ -8,3 +8,5 @@ helm repo index .
 export HELM_REGISTRY_REFERENCE="registry-1.docker.io/bougoucharts"
 
 yq eval -i '. |= .entries[][] |= .urls[0] = "oci://" + env(HELM_REGISTRY_REFERENCE) + "/" + .name + ":" + .version' index.yaml
+
+mv index.yaml ../
